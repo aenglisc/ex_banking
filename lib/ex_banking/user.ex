1b5ac2c9de2fe83e8adec34b9c30c9e19f4a0c6e
Roman Pushkov <pushkovroman@me.com>
2019-03-08 16:35:10 +0300

    improve guards

--- lib/ex_banking/user.ex
+++ lib/ex_banking/user.ex
@@ -9,7 +9,11 @@ defmodule ExBanking.User do
           :ok
           | {:error, :wrong_arguments}
           | {:error, :user_already_exists}
-  def create(user) when is_binary(user) do
+  def create(user) when not is_binary(user) do
+    {:error, :wrong_arguments}
+  end
+
+  def create(user) do
     with {:ok, producer} <- Producer.start_link(user),
          {:ok, _consumer} <- Consumer.start_link(producer) do
       :ok
@@ -18,15 +22,18 @@ defmodule ExBanking.User do
     end
   end
 
-  def create(_), do: {:error, :wrong_arguments}
-
   @spec get_balance(user, currency) ::
           {:ok, balance :: number}
           | {:error, :wrong_arguments}
           | {:error, :user_does_not_exist}
           | {:error, :too_many_requests_to_user}
   def get_balance(user, currency)
-      when is_binary(user) and is_binary(currency) do
+      when not is_binary(user)
+      when not is_binary(currency) do
+    {:error, :wrong_arguments}
+  end
+
+  def get_balance(user, currency) do
     case :global.whereis_name(user) do
       :undefined ->
         {:error, :user_does_not_exist}
@@ -36,17 +43,19 @@ defmodule ExBanking.User do
     end
   end
 
-  def get_balance(_, _), do: {:error, :wrong_arguments}
-
   @spec deposit(user, amount, currency) ::
           {:ok, new_balance :: number}
           | {:error, :wrong_arguments}
           | {:error, :user_does_not_exist}
           | {:error, :too_many_requests_to_user}
   def deposit(user, amount, currency)
-      when is_binary(user) and
-             is_number(amount) and amount > 0 and
-             is_binary(currency) do
+      when not is_binary(user)
+      when not is_binary(currency)
+      when not is_number(amount) or amount <= 0 do
+    {:error, :wrong_arguments}
+  end
+
+  def deposit(user, amount, currency) do
     case :global.whereis_name(user) do
       :undefined ->
         {:error, :user_does_not_exist}
@@ -56,8 +65,6 @@ defmodule ExBanking.User do
     end
   end
 
-  def deposit(_, _, _), do: {:error, :wrong_arguments}
-
   @spec withdraw(user, amount, currency) ::
           {:ok, new_balance :: number}
           | {:error, :wrong_arguments}
@@ -65,9 +72,13 @@ defmodule ExBanking.User do
           | {:error, :too_many_requests_to_user}
           | {:error, :not_enough_money}
   def withdraw(user, amount, currency)
-      when is_binary(user) and
-             is_number(amount) and amount > 0 and
-             is_binary(currency) do
+      when not is_binary(user)
+      when not is_binary(currency)
+      when not is_number(amount) or amount <= 0 do
+    {:error, :wrong_arguments}
+  end
+
+  def withdraw(user, amount, currency) do
     case :global.whereis_name(user) do
       :undefined ->
         {:error, :user_does_not_exist}
@@ -76,8 +87,6 @@ defmodule ExBanking.User do
         GenStage.call(pid, {:withdraw, amount, currency})
     end
   end
-
-  def withdraw(_, _, _), do: {:error, :wrong_arguments}
 
   @spec send(from_user :: user, to_user :: user, amount, currency) ::
           {:ok, from_user_balance :: number, to_user_balance :: number}
@@ -88,10 +97,15 @@ defmodule ExBanking.User do
           | {:error, :too_many_requests_to_receiver}
           | {:error, :not_enough_money}
   def send(from_user, to_user, amount, currency)
-      when is_binary(from_user) and
-             is_binary(to_user) and
-             is_number(amount) and amount > 0 and
-             is_binary(currency) do
+      when not is_binary(from_user)
+      when not is_binary(to_user)
+      when not is_binary(currency)
+      when not is_number(amount) or amount <= 0
+      when from_user == to_user do
+    {:error, :wrong_arguments}
+  end
+
+  def send(from_user, to_user, amount, currency) do
     case {:global.whereis_name(from_user), :global.whereis_name(to_user)} do
       {:undefined, _} ->
         {:error, :sender_does_not_exist}
@@ -99,13 +113,8 @@ defmodule ExBanking.User do
       {_, :undefined} ->
         {:error, :receiver_does_not_exist}
 
-      {pid, pid} ->
-        {:error, :wrong_arguments}
-
       {from_pid, _to_pid} ->
         GenStage.call(from_pid, {:send, to_user, amount, currency})
     end
   end
-
-  def send(_, _, _, _), do: {:error, :wrong_arguments}
 end
